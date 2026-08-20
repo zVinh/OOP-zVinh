eebef9397ffde069abb36b492a566418e52908f5
zVinh <thanhchanthien01@gmail.com>
2026-08-20 16:29:13 +0700

    Buoi_02

---
+++ OOP-zVinh/Buoi_02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // TODO: create array of Student objects
+        Student[] students = {
+                new Student("Le The Vinh", 8.5),
+                new Student("Phan Tan Phat", 7.2),
+                new Student("Pham Hung",9.0),
+                new Student("Le Phuong Trinh", 6.0),
+                new Student("Nguyen Thanh Dat",4.5)
+            };
+
+
+        // TODO: call static and instance methods as required
+        //Total Student
+        Console.WriteLine("Total Student " + Student.GetTotalStudents());
+        //List Student 
+
+        Console.WriteLine("List Student");
+        foreach (Student student in students)
+            Console.WriteLine("Name: " + student.GetName() +
+            ", Score: " + student.GetScore() +
+            ", Classification: " + student.GetClassification() +
+            ", Status: " + (student.IsPassed() ? "Pass" : "Fail")
+        );
+        //Top student
+        Student topStudent = Student.FindTopStudent(students);
+        Console.WriteLine("Top student " + topStudent.GetName() + " - " + topStudent.GetScore()
+            );
+        // Calculate average score
+        double averageScore = Student.CalculateAverageScore(students);
+
+        Console.WriteLine(
+                "\nClass average score: " +
+                averageScore.ToString("F2")
+            );
+    }
+
+}
---
+++ OOP-zVinh/Buoi_02/Student Management .cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+public class Student
+{
+    private string name;
+    private double score;
+    private static int totalStudents = 0;
+
+    public Student(string name, double score)
+    {
+        this.name = name;
+        this.score = score;
+        totalStudents++;
+    }
+
+    // TODO: write instance methods here
+    public string GetName()
+    {
+        return name;
+    }
+    public double GetScore()
+    {
+        return score;
+    }
+
+    public bool IsPassed()
+    {
+        if (score >= 5.0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public string GetClassification()
+    {
+        if (score >= 8.0)
+        {
+            return "Excellent";
+        }
+        else if (score >= 6.5)
+        {
+            return "Good";
+        }
+        else if (score >= 5.0)
+        {
+            return "Average";
+        }
+        else
+        {
+            return "Weak";
+        }
+    }
+    // TODO: write static methods here
+    public static int GetTotalStudents()
+    {
+        return totalStudents;
+    }
+    public static Student FindTopStudent(Student[] students)
+    {
+        Student topstudent = students[0];
+
+        foreach (Student student in students)
+        {
+            if (student.score > topstudent.score)
+            {
+                topstudent = student;
+            }
+        }
+
+        return topstudent;
+    }
+    public static double CalculateAverageScore(Student[] students)
+    {
+        double total = 0;
+        foreach (Student student in students)
+        {
+            total += student.score;
+
+        }
+        return total / students.Length;
+    }
+}
+
+    
