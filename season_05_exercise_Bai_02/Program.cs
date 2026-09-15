c3894952acff9fb2214c1519446cba439501ff79
zVinh <thanhchanthien01@gmail.com>
2026-09-16 03:43:25 +0700

    bài tập 5

---
+++ season_05_exercise_Bai_02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace season_05_exercise_Bai_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
